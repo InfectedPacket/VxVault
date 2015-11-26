09798ba44fe195d71330c29b8adc6e001ea306c9
InfectedPacket <infectedpacket@gmail.com>
2015-11-25 22:35:11 -0800

    Forked Shell into separate bin
    Forked Shell into separate bin

---
+++ vxvault-shell.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#█▀▀▀▀█▀▀▀▀▀██▀▀▀▀██▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▓▒▀▀▀▀▀▀▀▀▀▀█▓▀ ▀▀▀██▀▀▀▀▀▀▀▀▀▓▓▀▀▀▀▀▀▀▀▀▌
+#▌▄██▌ ▄▓██▄ ▀▄█▓▄▐ ▄▓█▓▓▀█ ▄▓██▀▓██▓▄ ▌▄█▓█▀███▓▄ ▌▄█▓█ ▀ ▄▓██▀▓██▓▄ ▄█▓█▀███▄■
+#▌▀▓█▓▐▓██▓▓█ ▐▓█▓▌▐▓███▌■ ▒▓██▌ ▓██▓▌▐▓▒█▌▄ ▓██▓▌ ▐▓▒█▌▐ ▒▓██▌  ▓██▓▌▓▒█▌ ▓█▓▌
+#▐▓▄▄▌░▓▓█▓▐▓▌ █▓▓▌░▓▓█▓▄▄ ▓▓██▓▄▄▓█▓▓▌░▓█▓ █ ▓█▓▓▌░▓█▓ ▒ ▓▓██▓▄▄▓█▓▓▌▓█▓ ░ ▓█▓▓
+#▐▓▓█▌▓▓▓█▌ █▓▐██▓▌▐▓▒▓▌ ▄ ▐░▓█▌▄ ▀▀▀ ▐▓▓▓ ▐▌ ▀▀▀  ▐▓▓▓▄▄ ▐░▓█▌ ▄ ▀▀▀ ▓▓▓ ░ ██▓▓
+#▐▓▓▓█▐▓▒██ ██▓▓▓▌▐▓▓██  █▌▐▓▓▒▌▐ ███░▌▐▓▓▒▌▐ ███░▌ ▐▓▓▒▌ ▐▓▓▒▌▀ ███░▌▓▓▒▌ ███░
+# ▒▓▓█▌▒▓▓█▌ ▐▓█▒▒  ▒▓██▌▐█ ▒▓▓█ ▐█▓▒▒ ▒▒▓█  ▐█▓▒▒  ▒▒▓█ ▓▌▒▓▓█ ▐█▓▒▒ ▒▒▓█ ▐█▓▒▌
+#▌ ▒▒░▀ ▓▒▓▀  ▀░▒▓ ▐▌ ▓▓▓▀ █ █▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀░█▓ ▄▌ ▒▒▓▀▀ █▒▓▀▀░█▓ ▒▒▓▀▀░█▀
+#█▄ ▀ ▄▄ ▀▄▄▀■ ▀ ▀▓█▄ ▀ ▄█▓█▄ ▀ ▓▄▄▄▄▄█▀ ▄▀ ▄▄▄▄▄▄█▓▄ ▀ ▄▄█▓▄▀ ▄▓▄█▄▀ ▄▄▄█▌
+#
+# Copyright (C) 2015 Jonathan Racicot
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http:#www.gnu.org/licenses/>.
+#
+# You are free to use and modify this code for your own software 
+# as long as you retain information about the original author
+# in your code as shown below.
+#
+# <author>Jonathan Racicot</author>
+# <email>infectedpacket@gmail.com</email>
+# <date>2015-03-26</date>
+# <url>https://github.com/infectedpacket</url>
+#//////////////////////////////////////////////////////////
+# Program Information
+#
+PROGRAM_NAME = "vxvault"
+PROGRAM_DESC = ""
+PROGRAM_USAGE = "%(prog)s [-i] [-h|--help] (OPTIONS)"
+
+__version_info__ = ('0','1','0')
+__version__ = '.'.join(__version_info__)
+
+#//////////////////////////////////////////////////////////
+# Imports Statements
+import os
+import argparse
+from Shell import *
+#//////////////////////////////////////////////////////////
+
+#//////////////////////////////////////////////////////////
+# Argument Parser Declaration
+#
+usage = PROGRAM_USAGE
+parser = argparse.ArgumentParser(
+	usage=usage, 
+	prog=PROGRAM_NAME, 
+	version="%(prog)s "+__version__, 
+	description=PROGRAM_DESC)
+	
+vault_options = parser.add_argument_group("Vault Options", "Sets basic options for the vault.")
+vault_options.add_argument("-b", "--base", 
+	dest="base", 
+	required=True,
+	default=os.getcwd(),
+	help="Specifies the location of the vault to use.")
+vault_options.add_argument("-vt", "--vtapi", 
+	dest="vtapikey", 
+	required=True,
+	help="Provides the public key to use the API of VirusTotal.")	
+vault_options.add_argument("-d", "--debug", 
+	dest="debug", 
+	action="store_true",
+	default=False,
+	help="Provides the public key to use the API of VirusTotal.")	
+#//////////////////////////////////////////////////////////
+
+def banner():
+    print("Copyright (C) 2015  Jonathan Racicot <jonathan.racicot@rmc.ca>")
+    print(
+    """
+    This program comes with ABSOLUTELY NO WARRANTY. This is
+    free software, and you are welcome to redistribute it
+    under certain conditions. See the GNU General Public
+    License v3 for more information. 
+    """)
+
+def main(args):
+	shell = Shell(_debug=args.debug)
+	shell.start(_base = args.base, 
+				_vtapi=args.vtapikey)
+
+if __name__ == "__main__":
+	banner()
+	main(parser.parse_args())
